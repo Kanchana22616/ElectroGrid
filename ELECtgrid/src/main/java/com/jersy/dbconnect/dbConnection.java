b4aa1d0704fea73a6edaaa26738ea8d8d6a16127
Harindu98 <hiranthadesilva11@gmail.com>
2022-04-22 23:55:53 +0530

    add database connection

---
+++ ELECtgrid/src/main/java/com/jersy/dbconnect/dbConnection.java
@@ -0,0 +1,21 @@
+package com.jersy.dbconnect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class dbConnection {
+
+	public static Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jersyproject", "root", "root2022");
+			// For testing
+			System.out.print("---Successfully connected DataBases---");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+}

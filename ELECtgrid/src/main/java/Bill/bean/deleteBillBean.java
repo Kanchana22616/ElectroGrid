edf7fde7097c2688a160e10b93241c6e692102f5
Kanchana <Kanchu22616@gmail.com>
2022-04-23 10:17:01 +0530

    add classes

---
+++ ELECtgrid/src/main/java/Bill/bean/deleteBillBean.java
@@ -0,0 +1,15 @@
+package Bill.bean;
+
+public class deleteBillBean {
+	
+	private String billId;
+
+	public String getBillId() {
+		return billId;
+	}
+
+	public void setBillId(String billId) {
+		this.billId = billId;
+	}
+	
+}

4c3429b9e93f85ab887cb7237d240134551f6acf
Harindu98 <hiranthadesilva11@gmail.com>
2022-04-25 20:23:03 +0530

    create search table

--- ELECtgrid/src/main/java/Employee/Dao/ViewEmployeeDao.java
+++ ELECtgrid/src/main/java/Employee/Dao/ViewEmployeeDao.java
@@ -19,9 +19,17 @@ public class ViewEmployeeDao {
 
 
 		Connection con = dbConnection.connect();   //conneting data base table 
-
+		String output = "";
 		try {
-
+			// Prepare the html table to be displayed here 
+			output = "<table border='1'><tr>"
+					+ "<th>EmpID</th>"
+					+ "<th> EmpName</th>"
+					+ "<th>EmpAge</th>"
+					+ "<th>EmpDept</th>"
+					+ "<th>EmpDOB</th>"
+					+ "<th>EmpEmail</th>"
+					+ "<th>ContactNUm</th> </tr>";
 			PreparedStatement ps1 = con.prepareStatement("select * from Employee where EmpID=?");
 			ps1.setInt(1, rs.getEmpID());
 		
@@ -30,18 +38,27 @@ public class ViewEmployeeDao {
 			if (rrs.next()) {
 
 					//API link overall attribute
-				JSONObject jsonObject = new JSONObject();
-				jsonObject.put("EmpID", rrs.getInt(1));
-				jsonObject.put("EmpName", rrs.getString(2));
-				jsonObject.put("EmpAge", rrs.getInt(3));
-				jsonObject.put("EmpDept", rrs.getString(4));
-				jsonObject.put("EmpDOB", rrs.getString(5));
-				jsonObject.put("EmpEmail", rrs.getString(6));
-				jsonObject.put("ContractNum", rrs.getString(7));
-				JSONArray jsonArray = new JSONArray();
-				jsonArray.add(jsonObject);
-				
-				return "" + jsonArray;    //taking the out put request
+				//JSONObject jsonObject = new JSONObject();
+				int empID = rrs.getInt(1);      //set up data as become table 
+				String empName = rrs.getString(2);
+				int empAge = rrs.getInt(3);
+				String empDept = rrs.getString(4);
+				String empDOB = rrs.getString(5);
+				String empEmail = rrs.getString(6);
+				String contractNum = rrs.getString(7);
+
+				// Add into the html table
+				output += "<tr><td>" + empID + "</td>";
+				output += "<td>" + empName + "</td>";
+				output += "<td>" + empAge + "</td>";
+				output += "<td>" + empDept + "</td>";
+				output += "<td>" + empDOB + "</td>";
+				output += "<td>" + empEmail + "</td>";
+				output += "<td>" + contractNum + "</td>";		;
+				//JSONArray jsonArray = new JSONArray();
+				//jsonArray.add(jsonObject);
+				output += " </table>"; 
+				return "" + output;    //taking the out put request
 				
 			
 
